a1c1064389f5ec159109f9ce8136392113619bb5
Patrick Ferguson <patrick@ferguson.cloud>
2017-06-10 22:08:19 +1000

    Use prop-types package instead of deprecated React.PropTypes import.
    Resolves warning: "Accessing PropTypes via the main React package is deprecated. ..."
    
    Also updated Radium to v0.19.1 to capture their prop-types package fix as well.

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 import { spring, TransitionMotion } from 'react-motion';
 import Radium from 'radium'
 
