a8fca40dbcfa2e3f9576c28cc6c5cb3a31ba95ba
catalin_covic <catalin.covic@gmail.com>
2019-07-10 19:34:59 -0700

    unified babelrc, added emotion

--- bin/webpack.config.js
+++ bin/webpack.config.js
@@ -28,24 +28,6 @@ const config = {
 					loader: 'babel-loader',
 					options: {
 						cacheDirectory: true,
-						babelrc: false,
-						presets: [
-							[
-								'@babel/preset-env',
-								{
-									targets: {
-										browsers: ['>0.5%', 'not ie 11', 'not op_mini all'],
-									},
-								},
-							],
-							'@babel/preset-typescript',
-							'@babel/preset-react',
-						],
-						plugins: [
-							// ["@babel/plugin-proposal-decorators", { legacy: true }],
-							['@babel/plugin-proposal-class-properties', { loose: true }],
-							'react-hot-loader/babel',
-						],
 					},
 				},
 			},
