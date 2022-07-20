2370d9b3b144e87e917ba7257e81a083bc1d98f1
sarmad <sarmad@onesingleview.com>
2022-07-20 16:27:45 +0530

    code updated

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
 /// <reference types="@emotion/react/types/css-prop" />
 import TimeKeeper from './components/TimeKeeperContainer'
-
 export default TimeKeeper
 
-export { TimeInput, TimeOutput } from './helpers/types'
