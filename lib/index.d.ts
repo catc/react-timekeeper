ec0d0f209ac14521fea2dd616c15f099c829089e
sarmad <sarmad@onesingleview.com>
2022-07-21 10:10:52 +0530

    code linked

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,2 +1,3 @@
 import TimeKeeper from './components/TimeKeeperContainer';
 export default TimeKeeper;
+export { TimeInput, TimeOutput } from './helpers/types';
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 /// <reference types="@emotion/react/types/css-prop" />
 import TimeKeeper from './components/TimeKeeperContainer'
+
 export default TimeKeeper
+
+export { TimeInput, TimeOutput } from './helpers/types'
 
